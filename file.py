5ea6ef2d115638b1ff4e464118de73f952fa427e
Ivan Maltsev <van.maltsev2005@yandex.ru>
2025-03-18 14:57:32 +0300

    v2

--- file.py
+++ file.py
@@ -1,10 +1,18 @@
 import time
 
 def sum(a, b):
-    start_time = time.time() 
-    result = a + b
-    end_time = time.time()
-    result_time = end_time - start_time
-    return f'Ответ: {result}, {result_time * 1000:.6f} милисекунд затрачено на решение'
+    return a + b
 
-print(sum(5, 10))
+def reverseArr(arr):
+    result = arr.reverse()
+    return arr
+
+def time_scan(func, *args1, **args2):
+    start_time = time.time()
+    result = func(*args1, **args2)
+    end_time = time.time()
+    result_time = (end_time - start_time) * 1000
+    return f'{result}, {result_time:.3f}'
+
+print(time_scan(sum, 5, 10))
+print(time_scan(reverseArr, [5, 6, 9]))
